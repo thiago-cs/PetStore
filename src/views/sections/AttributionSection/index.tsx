e59cb7d90956fcde69640451268aa5c757c975b1
Thiago Siqueira <thiagocs.cci@hotmail.com>
2022-12-14 02:52:37 -0300

    Wrapped content inside a section tag.
    Also added a header.

--- src/views/sections/AttributionSection/index.tsx
+++ src/views/sections/AttributionSection/index.tsx
@@ -1,3 +1,4 @@
+import { DuoToneHeader } from "views/components/DuoToneHeader";
 import { OpenInNewTab } from "views/icons";
 import { settings } from "../../../settings";
 
@@ -7,18 +8,22 @@ export function AttributionSection(): JSX.Element
 	const { attributions } = settings;
 
 	return (
-		<div className="" >
-		{
-			attributions.map((item, index) =>
-			<li key={index} >
-				<small className="p-1 flex items-center text-alt-low text-center hover:underline cursor-pointer" >
-					<a href={item.url} target="_blank" rel="noopener noreferrer" >
-						{item.content}
-					</a>
-					<OpenInNewTab size={20} />
-				</small>
-			</li>)
-		}
-		</div>
+		<section className="pt-20" >
+			<DuoToneHeader>{["Attr","ibutions"]}</DuoToneHeader>
+
+			<div>
+			{
+				attributions.map((item, index) =>
+				<li key={index} >
+					<small className="p-1 flex items-center text-alt-low text-center hover:underline cursor-pointer" >
+						<a href={item.url} target="_blank" rel="noopener noreferrer" >
+							{item.content}
+						</a>
+						<OpenInNewTab size={20} />
+					</small>
+				</li>)
+			}
+			</div>
+		</section>
 	);
 }
